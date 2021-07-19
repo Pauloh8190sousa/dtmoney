7943569729dea10375531a47fb51906b504ce021
Pauloh8190sousa <paulohenrique8190@gmail.com>
2021-07-19 12:39:29 -0300

    adicionando estilização no projeto com o styled-components e react

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { GlobalStyle } from "./styles/global";
 
 
 export function App() {
   return (
     <div className="App">
         <h1>Hello World</h1>
+        <GlobalStyle/>
     </div>
   );
 }
