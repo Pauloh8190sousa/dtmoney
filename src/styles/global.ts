53c0bd56e4cd5098304d5aaf2f77dcef8a04e456
Pauloh8190sousa <paulohenrique8190@gmail.com>
2021-07-19 16:25:26 -0300

    Criação e estilização de componentes

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,6 +5,7 @@ export const GlobalStyle = createGlobalStyle`
     :root{
         --background: #f0f2f5;
         --red: #E52E4D;
+        --green: #33CC95;
         --blue: #5429CC;
         --blue-light: #6933FF;
         --text-title: #363F5F;
@@ -31,6 +32,15 @@ export const GlobalStyle = createGlobalStyle`
     body{
         background: var(--background);
         -webkit-font-smoothing: antialiased;
+    }
+
+    body, input, textarea, button{
+        font-family: "Poppins", sans-serif;
+        font-weight: 400;
+    }
+    
+    h1, h2, h3, h4, h5 ,h6, strong{
+        font-weight: 600;
     }
 
     button{
