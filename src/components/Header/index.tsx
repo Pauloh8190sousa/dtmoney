811ca2806ae4321054d6ac73cc6c4003efbf8f1e
Pauloh8190sousa <paulohenrique8190@gmail.com>
2021-07-21 17:15:46 -0300

    criação do modal, form e estilização dos componentes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,17 @@
 import Logo from "../../assets/logo.svg";
 import { Container, Content } from "./styles";
 
-export function Header(){
+interface HeaderProps {
+    onOpenNewTransactionModal: () => void;
+}
+
+export function Header({onOpenNewTransactionModal}: HeaderProps){
+
     return (
         <Container>
             <Content>
                 <img src={Logo} alt="dt money" />
-                <button type="button">
+                <button type="button" onClick={onOpenNewTransactionModal}>
                     Nova transação
                 </button>
             </Content>
